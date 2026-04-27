1642ba65c39556ab474d3c40eff73c93e88cbd7c
Dorcu Eduard-Daniel <99557659+MaskBoy999@users.noreply.github.com>
2026-04-27 11:23:23 +0300

    Update Main.java

--- homework+advancedLab8/src/main/java/org/example/Main.java
+++ homework+advancedLab8/src/main/java/org/example/Main.java
@@ -3,8 +3,7 @@ package org.example;
 import javax.swing.*;
 import java.awt.*;
 import org.example.tools.*;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     static void main() {
             MainFrame frame = new MainFrame();
